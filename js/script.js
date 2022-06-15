94a8600394e6261a233925c448c8221062adccc1
vitalii <vitalik.yesyp@gmail.com>
2022-06-15 15:24:30 +0300

    make new console

--- js/script.js
+++ js/script.js
@@ -38,8 +38,28 @@ console.log(incr);
 console.log(decr);
 
 console.log(++incr);
-console.log(--decr); */
+console.log(--decr); 
 
 const negativeNumber = -25;
 
-console.log(negativeNumber);
+console.log(negativeNumber); */
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
