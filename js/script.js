f5743628fb2b37eb516d6918e66564bb19be8047
vitalii <vitalik.yesyp@gmail.com>
2022-06-20 14:15:22 +0300

    Second task

--- js/script.js
+++ js/script.js
@@ -92,9 +92,9 @@ for (let i = 1; i < 100; i++) {
         break;
     }
     console.log(i);
-} */
+} 
 
-/*
+
       *
      ***
     *****
@@ -125,4 +125,75 @@ for (let i = 0; i < b; i++) {
     a += '\n';
 }
 
-console.log(a); */
+console.log(a); 
+
+firstCicle: for (let i = 0; i < 3; i++) {
+    console.log(i);
+
+    secondCicle: for (let j = 0; j < 4; j++) {
+        console.log(j);
+        if (j === 2) {continue firstCicle;}
+
+        thirdCicle: for (let k = 0; k < 4; k++) {
+            console.log(k);
+            if (k === 2) {continue secondCicle;}
+        }
+    }
+}
+
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+
+for (let i = arr.length; i > 0; i--) {
+    let a = arr[arr.length - i];
+    result[arr.length - i] = a;
+}
+
+for (let i = 0; i < arr.length; i++) {
+    result[i] = arr[i];
+}
+
+console.log(arr);
+console.log(result);
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+for (let i = data.length; i > 0; i--) {
+    let b = 1;
+    let a = data[data.length - i];
+    if (typeof(a) === 'number') {
+        b = a * 2;
+        data[data.length - i] = b;
+    } else if (typeof(a) === 'string') {
+        a += ' - done';
+        data[data.length - i] = a;
+    }
+}
+
+
+for (let i = 0; i < data.length; i++) {
+    if (typeof(data[i]) === 'number') {
+        data[i] = data[i] * 2;
+    } else if (typeof(data[i]) === 'string') {
+        data[i] = `${data[i]} - done`;
+    }
+}
+console.log(data);
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+const result = [];
+let b = data.length - 1;
+
+for (let i = data.length; i > 0; i--) {
+    let a = data[b];
+    result[data.length - i] = a;
+    b--;
+}
+
+for (let i = 1; i <= data.length; i++) {
+    result[i - 1] = data[data.length - i]
+}
+
+console.log(data);
+console.log(result);*/
+
