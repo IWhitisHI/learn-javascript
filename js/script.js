1a6b17f943da3736920804fe7251b5e15cf962d4
vitalii <vitalik.yesyp@gmail.com>
2022-08-12 18:57:56 +0300

    Practicise 14

--- js/script.js
+++ js/script.js
@@ -492,4 +492,71 @@ function availableCurr(arr, missingCurr) {
 }
 
 showConsole(availableCurr([...baseCurrencies, ...additionalCurrencies], 'EUR'));
-*/
+
+
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+function isBudgetEnough(data) {
+    let square = 0,
+        volume = 0;
+    
+    data.shops.forEach(shop => {
+        square += shop.width * shop.length;
+    });
+
+    volume = square * data.height;
+
+    if (data.budget - (volume * data.moneyPer1m3) >= 0) {
+        return 'Бюджета достаточно';
+    } else {
+        return 'Бюджета недостаточно';
+    }
+}
+
+console.log(isBudgetEnough(shoppingMallData));
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam', 'Mark', 'Mark', 'Mark', 'Mark'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+
+    const a = [], b = [], c = [], rest = [];
+
+    for (let i = 0; i < arr.length; i++){
+        if (i < 3){
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }
+    }
+
+    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`];
+}
+
+showConsole(sortStudentsByGroups(students));*/
