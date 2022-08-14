b546e66d6706e109bde27fb52b1ad51dc4bd4ba0
vitalii <vitalik.yesyp@gmail.com>
2022-08-15 02:59:56 +0300

    Task search mark

--- js/script.js
+++ js/script.js
@@ -537,7 +537,8 @@ function isBudgetEnough(data) {
 
 console.log(isBudgetEnough(shoppingMallData));
 
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam', 'Mark', 'Mark', 'Mark', 'Mark'];
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard',
+ 'Takesi', 'Sam', 'Mark', 'Mark', 'Mark', 'Mark'];
 
 function sortStudentsByGroups(arr) {
     arr.sort();
@@ -559,4 +560,67 @@ function sortStudentsByGroups(arr) {
     return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`];
 }
 
-showConsole(sortStudentsByGroups(students));*/
+showConsole(sortStudentsByGroups(students));
+
+console.log(+'sadsafadsfda');
+
+showConsole(parseInt('13px', 3));
+
+const a = 1;
+showConsole(!!a);
+
+console.log( typeof(+"Infinity") );*/
+
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
+
+function isOpen(prop) {
+    let answer = '';
+    prop ? answer = 'Открыто' : answer = 'Закрыто';
+
+    return answer;
+}
+
+console.log(isOpen(restorantData.openNow));
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+    if ((+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1))) < +average.slice(0, -1)) {
+        return 'Цена ниже средней';
+    } else {
+        return 'Цена выше средней';
+    }
+}
+
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
+
+    copy.waitors =[ {name: 'Mike', age: 32} ];
+    return copy;
+}
+
+transferWaitors(restorantData);
